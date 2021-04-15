0c51d61ef830533c964af6470803b3f331bb6952
Syed Rifat Ahsan <syed.rifat.ahsan@gmail.com>
2021-04-16 01:27:35 +0600

    Basic example

---
+++ test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path=r"D:\Python\chromedriver.exe")
+driver.get("http://www.python.org")
+# assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("asdsa")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+# print(driver.title)
+# quit entire window
+# driver.quit()
